46d9bde33b786ab94181e4ec3621811a0092c6e7
Dan LaRocque <dalaro@hopcount.org>
2015-12-05 03:47:48 -0500

    BulkLoaderVertexProgram ID handling hack
    @dkuppitz has been experimenting with this change atop TINKERPOP-1025
    in a test environment with a customized, locally-installed
    3.1.1-SNAPSHOT build.  I'm committing this change onto TINKERPOP-1025
    with a custom version and tag so that I can test against it
    reproducibly.
    
    (cherry picked from commit c08eb20a4f0b4b9c47d52c3a33c9701db599c2bb)

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/computer/bulkloading/BulkLoaderVertexProgram.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/computer/bulkloading/BulkLoaderVertexProgram.java
@@ -217,7 +217,7 @@ public class BulkLoaderVertexProgram implements VertexProgram<Tuple> {
             this.commit(false);
             if (!bulkLoader.useUserSuppliedIds()) {
                 // create an id pair and send it to all the vertex's incoming adjacent vertices
-                sourceVertex.property(DEFAULT_BULK_LOADER_VERTEX_ID, targetVertex.id());
+                sourceVertex.property(DEFAULT_BULK_LOADER_VERTEX_ID, targetVertex.id().toString());
                 messenger.sendMessage(messageScope, Pair.with(sourceVertex.id(), targetVertex.id()));
             }
         } else if (memory.getIteration() == 1) {
